602196359b31d4344ac7e1a4fa174fc804052f15
Katrina Pingryn <katep@indeed.com>
2024-01-24 16:24:18 -0600

    initial solution

--- src/features/photos/create/index.js
+++ src/features/photos/create/index.js
@@ -1,13 +1,18 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 // Task 2: Import the `useDispatch()` method from the appropriate package
-// Task 3: Import the `addPhoto()` action creator from the photos slice
+import { useDispatch } from "react-redux";
 
-import './create.css';
+// Task 3: Import the `addPhoto()` action creator from the photos slice
+import { addPhoto } from "../photos.slice";
+
+import "./create.css";
 
 export default function CreatePhoto() {
-  const [formData, setFormData] = useState({ imageUrl: '', caption: '' });
+  const [formData, setFormData] = useState({ imageUrl: "", caption: "" });
+
   // Task 4: Store a reference to the Redux store's dispatch method in a variable called `dispatch`
+  const dispatch = useDispatch();
 
   function handleChange({ target: { name, value } }) {
     setFormData({
@@ -18,8 +23,10 @@ export default function CreatePhoto() {
 
   function handleSubmit(event) {
     event.preventDefault();
+
     // Task 5: Dispatch the `addPhoto()` action creator, passing in the form data
-    setFormData({ imageUrl: '', caption: '' });
+    dispatch(addPhoto(formData));
+    setFormData({ imageUrl: "", caption: "" });
   }
 
   return (
--- src/features/suggestion/suggestion.slice.js
+++ src/features/suggestion/suggestion.slice.js
@@ -1,12 +1,18 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
-export const fetchSuggestion =
-  createAsyncThunk(/* Task 15: Complete the `createAsyncThunk()` function to load a suggestion from this URL: http://localhost:3004/api/suggestion */);
+export const fetchSuggestion = createAsyncThunk(
+  'suggestion/fetchSuggestion',
+  async (arg, thunkAPI) => {
+    const response = await fetch('http://localhost:3004/api/suggestion');
+    const { data } = await response.json();
+    return data;
+  }
+);
 
 const initialState = {
   suggestion: '',
   loading: false,
-  error: true,
+  error: false,
 };
 
 const options = {
@@ -14,7 +20,19 @@ const options = {
   initialState,
   reducers: {},
   extraReducers: {
-    /* Task 16: Inside `extraReducers`, add reducers to handle all three promise lifecycle states - pending, fulfilled, and rejected - for the `fetchSuggestion()` call */
+    [fetchSuggestion.pending]: (state) => {
+      state.loading = true;
+      state.error = false;
+    },
+    [fetchSuggestion.fulfilled]: (state, { payload }) => {
+      state.suggestion = payload;
+      state.loading = false;
+      state.error = false;
+    },
+    [fetchSuggestion.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = true;
+    },
   },
 };
 
@@ -23,6 +41,7 @@ const suggestionSlice = createSlice(options);
 export default suggestionSlice.reducer;
 
 // Task 17: Create a selector, called `selectSuggestion`, for the `suggestion` state variable and export it from the file
+export const selectSuggestion = (state) => state.suggestion.suggestion;
 
 export const selectLoading = (state) => state.suggestion.loading;
 export const selectError = (state) => state.suggestion.error;
